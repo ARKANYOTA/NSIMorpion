9bf3e2fed40fbfb1540cce60916f9e57412ea14f
ARKANYOTA <38374187+ARKANYOTA@users.noreply.github.com>
2021-12-15 19:43:47 +0000

    V2: Debut pygame

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ TODO:
 -
 '''
 import pygame
+from pygame.locals import *
+
 
 
 class Player:
@@ -82,7 +84,7 @@ class Jeu:
         boucleQuelleCase = True
         while boucleQuelleCase:
             print(str(self.grille))
-            currentCase = input(str(self.joueurs[self.quiJoue])+": Quelle case jouer? : ")
+            currentCase = input(str(self.joueurs[self.quiJoue])+": Quelle case jouer? entre [0;8] : ")
             if currentCase.isdigit():
                 currentCase = int(currentCase)
                 if 0 <= currentCase <= 8:
@@ -101,21 +103,92 @@ class Jeu:
     def jouer(self):
         while not self.grille.isGagnant():
             self.tourSuivant()
+
         print(str(self.grille))
-        print(f"{self.joueur().name} a gagné!")
+        self.quiJoue = not self.quiJoue
+        print(f"{str(self.joueur())} a gagné!")
 
+def image(name, rotate=0, size=(32, 32)):
+    return pygame.transform.rotate(pygame.transform.scale(pygame.image.load("./images/" + name + ".png"), size), rotate)
 
+def changeMenu(menu, sprites):
+    # menu:  0= Menu principal, 1=Selection de partie(Multi), 2=Selection de partie(Local), 3=
+    if menu == 0:
+        # Afficher les bouttons du menu principal
+        sprites.append(Sprite((50, 50), (1000, 1000), image("buttonJouer"), "jouer"))
+        pass
+    elif menu == 1:
+        # Afficher la selection de partie (multi)
+        pass
+    elif menu == 2:
+        # Afficher la selection de partie (local)
+        pass
+    elif menu == 3:
+        # Afficher le menu config IA
+        pass
+    elif menu == 4:
+        # Afficher le jeu
+        pass
 
 
 
 
 def main():
-    jeu = Jeu(Player("J1", "X"), Player("J2", "O"))
-    jeu.jouer()
+    while True:
+        joueur1 = Player(input("Joueur 1: Quel est ton nom? : "), "X")
+        joueur2 = Player(input("Joueur 2: Quel est ton nom? : "), "O")
+        jeu = Jeu(joueur1, joueur2)
+        jeu.jouer()
+        if input("Voulez-vous rejouer? [y/n] : ") == "y":
+            continue
+        else:
+            break
+
+
+def main_pygame():
+    pygame.init()
+    screen = pygame.display.set_mode((600, 600))
+    pygame.display.set_caption("PyGame")
+    screen.fill((255, 255, 255))
+    running = True
+    sprites = []
+    witchMenu = 0  # 0 = menu principal, 1 = Menu de selection, 2 = Menu de jeu
+    oldMenu = -1
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        if witchMenu != oldMenu:  # Menu de Jeu
+            changeMenu(witchMenu, sprites)
+            oldMenu = witchMenu
+
+        for sprite in sprites:
+            screen.blit(sprite.image, sprite.pos)
+
+        pygame.display.update()
+    pygame.quit()
+
+
+class Sprite(Rect):
+    def __init__(self, pos, size, image, name="null", collide=False):
+        super().__init__(pos[0], pos[1], size[0], size[1])
+        self.image = image
+        self.pos = pos
+        self.name = name
+        self.collide = collide
+
+    def move(self, x: float, y: float) -> Rect:
+        self.pos[0] = x
+        self.pos[1] = y
+
+    def isClicked(self):
+        return self.topleft[0] <= pygame.mouse.get_pos()[0] <= self.bottomright[0] and self.topleft[1] <= pygame.mouse.get_pos()[1] <= self.bottomright[1]
+
 
 
 if __name__ == '__main__':
-    main()
+    main_pygame()
 
 
 
